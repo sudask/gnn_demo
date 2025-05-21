21e66420f674ec17c667ed6ce89c7856cd590a37
sudask <02yimingwang@gmail.com>
2025-05-21 11:50:58 +0800

    cleaned up code

--- field/main.py
+++ field/main.py
@@ -5,16 +5,37 @@ from train import*
 from read_and_save_ncdata import*
 from torch.optim.lr_scheduler import StepLR
 
-BLOCK_SIZE = 80 # differenct size should use different model
-NUM_TRAINING_DATA = 150
 
-# random sample
-# random.seed(42)
-numbers = list(range(180))
-training_indices = random.sample(numbers, NUM_TRAINING_DATA)
-testing_indices = [i for i in numbers if i not in set(training_indices)]
+# ======================== generate indices of different sets ========================
 
-# load data
+random.seed(42)
+
+BLOCK_SIZE = 80 # different size should use different model
+NUM_DATA = 200
+
+# split ratio
+train_ratio = 0.6
+validate_ratio = 0.2
+test_ratio = 0.2
+
+# Calculate sizes
+train_size = int(NUM_DATA * train_ratio)
+validate_size = int(NUM_DATA * validate_ratio)
+test_size = NUM_DATA - train_size - validate_size
+
+print(f"Amount of traning data: {train_size}")
+print(f"Amount of validation data: {validate_size}")
+print(f"Amount of testing data: {test_size}")
+
+numbers = list(range(NUM_DATA))
+
+training_indices = random.sample(numbers, train_size)
+remaining_indices = [i for i in numbers if i not in set(training_indices)]
+validation_indices = random.sample(remaining_indices, validate_size)
+testing_indices = [i for i in remaining_indices if i not in set(validation_indices)]
+
+# ======================== load data from npy files ========================
+
 all_lat = np.load("data/lat.npy").astype(np.float32) # 320
 all_lon = np.load("data/lon.npy").astype(np.float32) # 416
 all_station = np.load("data/station_pos.npy").astype(np.float32) # 1754 * 2
@@ -23,10 +44,11 @@ all_val = np.load("data/vals.npy").astype(np.float32) # 1995 * 320 * 416
 all_obs = np.load("data/obs.npy").astype(np.float32) # 1995 * 1754
 all_time = np.load("data/time.npy").astype(np.float32) # 1995 * 1
 
-# prepare data
+# ======================== use indices to genearte data sets ========================
+
 lat = all_lat[30:30 + BLOCK_SIZE]
 lon = all_lon[120:120 + BLOCK_SIZE]
-# use 1500 time
+
 real_vals = all_val[training_indices, 30:30 + BLOCK_SIZE, 120:120 + BLOCK_SIZE]
 # choose obs stations belong to required range
 valid_indices = np.where((all_station[:, 0] <= lat[-1]) & (all_station[:, 1] <= lon[-1]) & (all_station[:, 0] >= lat[0]) & (all_station[:, 1] >= lon[0]))[0]
@@ -38,13 +60,30 @@ edge_index = generateEdgeIndex(obs_station)
 # plotObs(lat, lon, obs_station)
 
 training_data = []
-for i in range(NUM_TRAINING_DATA):
-    obs_reshaped = obs[i].reshape(-1, 1)
+validation_data = []
+testing_data = []
+
+for i in training_indices:
+    obs_reshaped = all_obs[i, valid_indices].reshape(-1, 1)
     feature = torch.from_numpy(np.concatenate((obs_reshaped, obs_station), axis=1))
-    #--- for no lat and lon info
+    # no lat and lon info:
     # feature = torch.from_numpy(obs[i]).reshape(-1, 1)
-    vals = torch.from_numpy(real_vals[i].reshape(-1))
+    vals = torch.from_numpy(all_val[i, 30:30 + BLOCK_SIZE, 120:120 + BLOCK_SIZE].reshape(-1))
     training_data.append(MyData(feature, torch.from_numpy(edge_index), vals))
+
+for i in validation_indices:
+    obs_reshaped = all_obs[i, valid_indices].reshape(-1, 1)
+    feature = torch.from_numpy(np.concatenate((obs_reshaped, obs_station), axis=1))
+    vals = torch.from_numpy(all_val[i, 30:30 + BLOCK_SIZE, 120:120 + BLOCK_SIZE].reshape(-1))
+    validation_data.append(MyData(feature, torch.from_numpy(edge_index), vals))
+
+for i in testing_indices:
+    obs_reshaped = all_obs[i, valid_indices].reshape(-1, 1)
+    feature = torch.from_numpy(np.concatenate((obs_reshaped, obs_station), axis=1))
+    vals = torch.from_numpy(all_val[i, 30:30 + BLOCK_SIZE, 120:120 + BLOCK_SIZE].reshape(-1))
+    testing_data.append(MyData(feature, torch.from_numpy(edge_index), vals))
+
+# ======================== choose model ========================
 
 model = None
 if (obs.shape[1] == 94):
@@ -55,26 +94,27 @@ if (obs.shape[1] == 415):
 
 if (obs.shape[1] == 19):
     model = model19()
+
+# ======================== set nessesary components ========================
+
 optimizer = optim.Adam(model.parameters(), lr=0.1)
 scheduler = StepLR(optimizer, step_size=50, gamma=0.7)
 criterion = nn.MSELoss()
 
+# ======================== traing and svae model ========================
+
 trainAndPlot(model, training_data, optimizer, scheduler, criterion)
+
+# ======================== display results ========================
 
 checkpoint = torch.load("checkpoints/test.pth", weights_only=True)
 model.load_state_dict(checkpoint)
 
 total_error = np.zeros(BLOCK_SIZE ** 2)
-for i in testing_indices:
-    obs_reshaped = all_obs[i, valid_indices].reshape(-1, 1)
-    feature = torch.from_numpy(np.concatenate((obs_reshaped, obs_station), axis=1))
-    #--- for no lat and lon info
-    # feature = torch.from_numpy(all_obs[i, valid_indices]).reshape(-1, 1)
-    testing_data = MyData(feature, torch.from_numpy(edge_index))
-
-    predict = model(testing_data)
+for data in testing_data:
+    predict = model(data)
     predict_val = predict.detach().numpy()
-    real_val = all_val[i, 30:30 + BLOCK_SIZE, 120:120 + BLOCK_SIZE].reshape(-1)
+    real_val = data.vals.detach().numpy()
 
     error = (real_val - predict_val) ** 2
     total_error += error
